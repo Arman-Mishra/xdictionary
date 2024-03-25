c773fb0cd97c7f388f38ede21540b10966666caa
Arman-Mishra <armanmishra.sky@gmail.com>
2024-03-25 14:43:08 +0530

    Added changes to xdictionary

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ function App() {
         onChange={(e) => setWord(e.target.value)}
       />
       <button onClick={handleClick}>Search</button>
-      <p style={{ marginTop: "10px", fontWeight: "bold" }}>Defintion:</p>
+      <p style={{ marginTop: "10px", fontWeight: "bold" }}>Definition:</p>
       {definition ? <p>{definition}</p> : <></>}
     </div>
   );
